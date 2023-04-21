9658fdcae676e83aed41300d28c2356782a5c43d
kaushik vaghani <kaushik.vaghani111@gmail.com>
2023-04-21 06:30:36 +0100

    Update app.py
    edit some error code

--- Backend/HouseRentalService/app.py
+++ Backend/HouseRentalService/app.py
@@ -1,14 +1,28 @@
-from flask import Flask, request, jsonify, g, abort
+from flask import Flask, session, request, jsonify, g, abort
 from flask_cors import CORS, cross_origin
 from flask_mysqldb import MySQL
 import jwt
 from functools import wraps
 import os
-from flask_mail import Mail
+from flask_mail import Mail, Message
+
+from flask_session import Session
+import jwt
+import secrets
+
+
 
 mysql = MySQL()
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+app.secret_key = "eyJhbGciOiJIUzI1NiJ9.ew0KICAic3ViIjogIjEyMzQ1Njc4OTAiLA0KICAibmFtZSI6ICJBbmlzaCBOYXRoIiwNCiAgImlhdCI6IDE1MTYyMzkwMjINCn0.X3ZBXE9ycu4xJ4pPHc9QMP8gsOIe0Dez00F-m6Y6NAU"
+
+# CORS(app, resources={r"/*": {"origins": "*"}})
+
+# CORS(app,supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 @app.route('/api', methods=['POST'])
 def api():
@@ -23,11 +37,26 @@ app.config['MYSQL_DB'] = 'housepro'
 app.config['MYSQL_HOST'] = 'localhost' 
 mysql.init_app(app)
 
+
+# mail configuration
+
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'sampledbsproject@outlook.com'
+app.config['MAIL_PASSWORD'] = 'Dublin@14'
+mail = Mail(app)
+
+mail = Mail(app)
+
 # Generate a random string of characters
 secret_key = os.urandom(32)
 
 # Secret key for JWT
-app.config['SECRET_KEY'] = "eyJhbGciOiJIUzI1NiJ9.ew0KICAic3ViIjogIjEyMzQ1Njc4OTAiLA0KICAibmFtZSI6ICJBbmlzaCBOYXRoIiwNCiAgImlhdCI6IDE1MTYyMzkwMjINCn0.X3ZBXE9ycu4xJ4pPHc9QMP8gsOIe0Dez00F-m6Y6NAU"
+
+key=secrets.token_hex(16)
+app.config['SECRET_KEY'] = key
 
 def authenticate(f):
     @wraps(f)
@@ -66,29 +95,61 @@ def login():
     if result is None:
         abort(401)
     else:
-        return jsonify({"login": "success"}), 200
+
+        owner_id = result[0]
+        session['owner_id'] = owner_id
+        token = jwt.encode({'sub': owner_id}, app.config['SECRET_KEY'], algorithm='HS256')
+
+        return jsonify({'token': token}), 200
 
 
-@app.route('/properties', methods=['POST'])
-@authenticate
-def create_property():
+@app.route("/property", endpoint='property', methods=['POST'])
+# @authenticate
+def add_property():
 
-    # Create a new property listing.
+    
+
+    auth_header = request.headers.get('Authorization')
+    # if g.current_user is None:
+    #     return jsonify({'error': 'Unauthorized.'}), 401
+
+    if not auth_header:
+        return jsonify({'error': 'Authorization header missing.'}), 401
+    
+    try:
+        token = auth_header.split(' ')[1]
+        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+    except:
+        return jsonify({'error': 'Invalid token.'}), 401
+    
+    if not session.get('owner_id'):
+        return jsonify({'error': 'User ID missing from token.'}), 401
+
+
     data = request.get_json()
+    print(data)
+    # Extract the data from the request
+    owner_id = session.get('owner_id')
+    property_type = data['property_type']
+    address = data['address']
+    city=data['city']
+    bedrooms = data['bedrooms']
+    description = data['description']
+    price = data['price']
+    url = data['url']
+    
 
-    # Insert property listing into MySQL database with current user ID
+    # Query MySQL database to insert the new property
     cursor = mysql.connection.cursor()
-    cursor.execute("INSERT INTO properties (owner_id, address, bedrooms, bathrooms, price) VALUES (%s, %s, %s, %s, %s)",
-                   (g.current_user, data['address'], data['bedrooms'], data['bathrooms'], data['price']))
-    mysql.commit()
+    cursor.execute("INSERT INTO properties (owner_id, address,city, bedrooms, description, price, url, property_type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (owner_id, address, city, bedrooms, description, price, url, property_type))
+    mysql.connection.commit()
     cursor.close()
 
-    return jsonify({'success': True}), 201
+    # Return a success message
+    return jsonify({"add_property": "success"}), 200
 
 
-
-
-@app.route('/properties', methods=['GET'])
+@app.route('/properties',endpoint='properties', methods=['GET'])
 def get_properties():
     
     # Retrieve all property listings.
@@ -98,111 +159,58 @@ def get_properties():
     results = cursor.fetchall()
     cursor.close()
 
-    return jsonify(results), 200
+    return jsonify({"properties": results}), 200
+
+@app.route("/existing-property-endpoint/<int:property_id>", endpoint='existing_property', methods=['GET'])
+def get_existing_property(property_id):
+    # your code here
+
+    # Query MySQL database for the property with the given ID
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM properties WHERE id=%s", (property_id,))
+    result = cursor.fetchone()
+    cursor.close()
+
+    # If the property does not exist, return an error message
+    if result is None:
+        return jsonify({"error": "Property does not exist"}), 404
+
+    # Otherwise, return the property as a JSON response
+    return jsonify({"property": result}), 200
 
 
-@app.route("/update_property", endpoint='update_property', methods=['PUT'])
-def update_property():
+@app.route("/register", endpoint='register', methods=['POST'])
+def register():
 
     data = request.get_json()
     print(data)
 
     # Extract the data from the request
-    property_id = data['property_id']
-    owner_id = data['owner_id']
-    address = data['address']
-    bedrooms = data['bedrooms']
-    description = data['description']
-    price = data['price']
-    url = data['url']
-    property_type = data['property_type']
+    name = data['name']
+    email = data['email']
+    password = data['password']
+    phone = data['phone']
 
-    # Query MySQL database to check if the property exists
+
+    # Query MySQL database to check if the email already exists
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM properties WHERE id=%s", (property_id,))
+    cursor.execute("SELECT * FROM house_owners WHERE email=%s", (email,))
     result = cursor.fetchone()
 
-    # If the property does not exist, return an error message
-    if result is None:
+    # If the email already exists, return an error message
+    if result is not None:
         cursor.close()
-        return jsonify({"error": "Property does not exist"}), 404
+        return jsonify({"error": "Email already exists"}), 400
 
-    # Otherwise, update the property in the database
-    cursor.execute("UPDATE properties SET owner_id=%s, address=%s, bedrooms=%s, description=%s, price=%s, url=%s, type=%s WHERE id=%s", (owner_id, address, bedrooms, description, price, url, property_type, property_id))
+    # Otherwise, insert the new house owner data into the database
+    cursor.execute("INSERT INTO house_owners (name, email, password, phone) VALUES (%s, %s, %s, %s)",
+                   (name, email, password, phone))
     mysql.connection.commit()
     cursor.close()
 
     # Return a success message
-    return jsonify({"update_property": "success"}), 200
+    return jsonify({"registration": "success"}), 200
 
-@app.route("/submit_property_request", endpoint='submit_property_request', methods=['POST'])
-def submit_property_request():
-
-    data = request.get_json()
-    property_id = data['property_id']
-    name = data['name']
-    email = data['email']
-    phone = data['phone']
-    description_message = data['description_message']
-
-    # Query the MySQL database to get the owner's email address
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT o.email FROM properties p JOIN house_owners o ON p.owner_id=o.id WHERE p.id=%s", (property_id,))
-    result = cursor.fetchone()
-    cursor.close()
-
-    if result is None:
-        abort(404)
-
-    owner_email = result[0]
-
-    # Send an email notification to the property owner
-    # You can use a library like Flask-Mail to send emails
-    # Here is an example using Flask-Mail
-    msg = Message(subject="New Property Request",
-                  sender="noreply@example.com",
-                  recipients=[owner_email])
-    msg.body = f"You have a new property request for property ID {property_id}. \n\nName: {name}\nEmail: {email}\nPhone: {phone}\n\nMessage:\n{description_message}"
-    mail.send(msg)
-
-    # Insert the property request into the MySQL database
-    cursor = mysql.connection.cursor()
-    cursor.execute("INSERT INTO property_request (property_id, name, email, phone, description_message) VALUES (%s, %s, %s, %s, %s)",
-                   (property_id, name, email, phone, description_message))
-    mysql.connection.commit()
-    cursor.close()
-
-    return jsonify({"message": "Property request submitted successfully."}), 200
-
-@app.route("/filter_properties", endpoint='filter_properties', methods=['GET'])
-def filter_properties():
-
-    # Get the filter query parameters
-    property_type = request.args.get('type')
-    city = request.args.get('city')
-
-    # Build the SQL query based on the filter parameters
-    sql_query = "SELECT * FROM properties"
-    query_params = []
-    if property_type is not None or city is not None:
-        sql_query += " WHERE"
-        if property_type is not None:
-            sql_query += " type=%s"
-            query_params.append(property_type)
-        if city is not None:
-            if len(query_params) > 0:
-                sql_query += " AND"
-            sql_query += " city=%s"
-            query_params.append(city)
-
-    # Query MySQL database for properties that match the filter criteria
-    cursor = mysql.connection.cursor()
-    cursor.execute(sql_query, tuple(query_params))
-    result = cursor.fetchall()
-    cursor.close()
-
-    # Return the matching properties as a JSON response
-    return jsonify({"properties": result}), 200
 
 @app.route("/change_password", endpoint='change_password', methods=['POST'])
 def change_password():
@@ -240,6 +248,41 @@ def change_password():
     # Return a success message
     return jsonify({"change_password": "success"}), 200
 
+@app.route("/update_property", endpoint='update_property', methods=['PUT'])
+def update_property():
+
+    data = request.get_json()
+    print(data)
+
+    # Extract the data from the request
+    property_id = data['property_id']
+    owner_id = data['owner_id']
+    address = data['address']
+    bedrooms = data['bedrooms']
+    description = data['description']
+    min_rent = data['min_rent']
+    max_rent = data['max_rent']
+    url = data['url']
+    property_type = data['property_type']
+
+    # Query MySQL database to check if the property exists
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM properties WHERE id=%s", (property_id,))
+    result = cursor.fetchone()
+
+    # If the property does not exist, return an error message
+    if result is None:
+        cursor.close()
+        return jsonify({"error": "Property does not exist"}), 404
+
+    # Otherwise, update the property in the database
+    cursor.execute("UPDATE properties SET owner_id=%s, address=%s, bedrooms=%s, description=%s, min_rent=%s, max_rent=%s, url=%s, type=%s WHERE id=%s", (owner_id, address, bedrooms, description, min_rent, max_rent, url, property_type, property_id))
+    mysql.connection.commit()
+    cursor.close()
+
+    # Return a success message
+    return jsonify({"update_property": "success"}), 200
+
 @app.route("/delete_property", endpoint='delete_property', methods=['DELETE'])
 def delete_property():
 
@@ -267,6 +310,25 @@ def delete_property():
     # Return a success message
     return jsonify({"delete_property": "success"}), 200
 
+@app.route("/my_properties", endpoint='my_properties', methods=['GET'])
+def view_my_properties():
+
+    # Get the authenticated house owner's ID from the session
+    owner_id = session.get('owner_id', None)
+
+    # If the house owner is not logged in, return an error message
+    if owner_id is None:
+        return jsonify({"error": "Not logged in"}), 401
+
+    # Query MySQL database for the house owner's properties
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM properties WHERE owner_id=%s", (owner_id,))
+    result = cursor.fetchall()
+    cursor.close()
+
+    # Return the properties as a JSON response
+    return jsonify({"properties": result}), 200
+
 @app.route("/search_properties", endpoint='search_properties', methods=['GET'])
 def search_properties():
 
@@ -281,6 +343,75 @@ def search_properties():
 
     # Return the matching properties as a JSON response
     return jsonify({"properties": result}), 200
+
+@app.route("/filter_properties", endpoint='filter_properties', methods=['GET'])
+def filter_properties():
+
+    # Get the filter query parameters
+    property_type = request.args.get('type')
+    city = request.args.get('city')
+
+    # Build the SQL query based on the filter parameters
+    sql_query = "SELECT * FROM properties"
+    query_params = []
+    if property_type is not None or city is not None:
+        sql_query += " WHERE"
+        if property_type is not None:
+            sql_query += " type=%s"
+            query_params.append(property_type)
+        if city is not None:
+            if len(query_params) > 0:
+                sql_query += " AND"
+            sql_query += " city=%s"
+            query_params.append(city)
+
+    # Query MySQL database for properties that match the filter criteria
+    cursor = mysql.connection.cursor()
+    cursor.execute(sql_query, tuple(query_params))
+    result = cursor.fetchall()
+    cursor.close()
+
+    # Return the matching properties as a JSON response
+    return jsonify({"properties": result}), 200
+
+@app.route("/submit_property_request", endpoint='submit_property_request', methods=['POST'])
+def submit_property_request():
+
+    data = request.get_json()
+    property_id = data['property_id']
+    name = data['name']
+    email = data['email']
+    phone = data['phone']
+    description_message = data['description_message']
+
+    # Query the MySQL database to get the owner's email address
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT o.email FROM properties p JOIN house_owners o ON p.owner_id=o.id WHERE p.id=%s", (property_id,))
+    result = cursor.fetchone()
+    cursor.close()
+
+    if result is None:
+        abort(404)
+
+    owner_email = result[0]
+
+    # Send an email notification to the property owner
+    # You can use a library like Flask-Mail to send emails
+    # Here is an example using Flask-Mail
+    msg = Message(subject="New Property Request",
+                  sender=(name, email),
+                  recipients=[owner_email])
+    msg.body = f"You have a new property request for property ID {property_id}. \n\nName: {name}\nEmail: {email}\nPhone: {phone}\n\nMessage:\n{description_message}"
+    mail.send(msg)
+
+    # Insert the property request into the MySQL database
+    cursor = mysql.connection.cursor()
+    cursor.execute("INSERT INTO property_request (property_id, name, email, phone, description_message) VALUES (%s, %s, %s, %s, %s)",
+                   (property_id, name, email, phone, description_message))
+    mysql.connection.commit()
+    cursor.close()
+
+    return jsonify({"message": "Property request submitted successfully."}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
